0c432a12b8cf0a1b3055ca7bb8b5e98cdbce54a7
Matthew Flickinger <mflick@umich.edu>
2015-08-27 16:29:40 -0400

    code formatting

--- scripts/locuszoom.js
+++ scripts/locuszoom.js
@@ -24,7 +24,7 @@ LocusZoom.initD3Viewer = function(holder, region) {
 		.attr("width", width)
 		.attr("height", height)
 		.append("svg:g")
-		.attr("transform", "translate(" + margin.left +  "," + margin.top + ")")
+		.attr("transform", "translate(" + margin.left +  "," + margin.top + ")");
 	
 	width = width - margin.left - margin.right;
 	height = height - margin.top - margin.bottom;
@@ -97,21 +97,21 @@ LocusZoom.initCanvasViewer = function(holder, region) {
 
 			x.log10pval = LocusZoom.Transform.NegLog10(x.pvalue);
 
-	    	x.ld = LocusZoom.SortedLeftJoin(
-	    		new LocusZoom.JSONIterator(x, function(z, i) {
-	    			if(i < z.chr.length) {
-	    				return({chr:z.chr[i], pos:z.position[i]})
-	    			} else {
-	    				return(null)
-	    			}
-	    		}, function(z, i) {return(0)}),
-	    		new LocusZoom.JSONIterator(y, function(z, i) {
-					return({chr:z.chromosome2[i],pos:z.position2[i]})
-				}, function(z, i) {return(z.rsquare[i])}),
-	    		LocusZoom.GenomePosComparer,
-	    		new LocusZoom.ArrayAccumulator()
-	    	);
-	    	
+			x.ld = LocusZoom.SortedLeftJoin(
+				new LocusZoom.JSONIterator(x, function(z, i) {
+					if(i < z.chr.length) {
+						return({chr:z.chr[i], pos:z.position[i]});
+					} else {
+						return(null);
+					}
+				}, function() {return(0);}),
+				new LocusZoom.JSONIterator(y, function(z, i) {
+					return({chr:z.chromosome2[i],pos:z.position2[i]});
+				}, function(z, i) {return(z.rsquare[i]);}),
+				LocusZoom.GenomePosComparer,
+				new LocusZoom.ArrayAccumulator()
+			);
+			
 
 			//b.setDataRange(0,10,0,10)
 			//b.setDisplayRange(0,dz.width*2, dz.height*2,0);
@@ -122,8 +122,8 @@ LocusZoom.initCanvasViewer = function(holder, region) {
 				
 			b.render(out);
 		}).done();
-	})
-}
+	});
+};
 
 LocusZoom.Data = {};
 LocusZoom.Data.LZAPI = function() {
@@ -132,21 +132,20 @@ LocusZoom.Data.LZAPI = function() {
 
 	var mergeLD = function(x, chr, start, end) {
 		this.getRefLD(2, refvar, chr, start ,end).then(
-			function(y) {
-	
-			}
-		)
-	}
+			function(y) {}
+		);
+	};
 
-    this.fetchResults = function(analysis, chr, start, end) {
+	this.fetchResults = function(analysis, chr, start, end) {
 		var url = "/api/v1/single/results?filter=analysis in 1 " + 
 			"and chromosome in  '" + chr + "'" + 
 			" and position ge " + start + 
-			" and position le " + end
+			" and position le " + end;
 		console.log(url);
-		var ret = LocusZoom.createCORSPromise("GET",url).then(function(x) {result = x; return(result)});
+		var ret = LocusZoom.createCORSPromise("GET",url)
+			.then(function(x) {result = x; return(result);});
 		return(ret);
-	}
+	};
 
 	this.getRefLD = function(pop, refvar, chr, start, end) {
 		var url = "/api/v1/pair/LD/results?find=reference::2" + 
@@ -157,8 +156,8 @@ LocusZoom.Data.LZAPI = function() {
 			"&fields=chr,pos,rsquare";
 		console.log(url);
 		return(LocusZoom.createCORSPromise("GET",url));
-	}
-}
+	};
+};
 
 /*
 LocusZoom.Data.UM.ResultsFacade = function(x) {
@@ -177,18 +176,18 @@ LocusZoom.Data.UM.ResultsFacade = function(x) {
 LocusZoom.Data.StaticLocal = function() {
 
 	this.fetchResults = function(analysis, chr, start, end) {
-		var url = "staticdata/pval.json"
-		console.log(url)
+		var url = "staticdata/pval.json";
+		console.log(url);
 		return(LocusZoom.createCORSPromise("GET",url));
 
-	}
+	};
 	this.getRefLD = function(analysis, chr, start, end) {
-		var url = "staticdata/ld.json"
-		console.log(url)
+		var url = "staticdata/ld.json";
+		console.log(url);
 		return(LocusZoom.createCORSPromise("GET",url));
 
-	}
-}
+	};
+};
 
 LocusZoom.Transform = {};
 LocusZoom.Transform.NegLog10 = function(x) {
@@ -197,12 +196,11 @@ LocusZoom.Transform.NegLog10 = function(x) {
 		out.push(-Math.log(x[i]) / Math.LN10);
 	}
 	return(out);
-}
+};
 
 LocusZoom.SortedLeftJoin = function(iteratorA, iteratorB, comparer, accumulator) {
 
 	var comp;
-	var i = 0;
 	while(true) {
 		comp = comparer(iteratorA.getKey(),iteratorB.getKey());
 		if ( iteratorA.isDone() ) {
@@ -220,74 +218,75 @@ LocusZoom.SortedLeftJoin = function(iteratorA, iteratorB, comparer, accumulator)
 		}
 
 	}
-	return(accumulator.getValues())
-}
+	return(accumulator.getValues());
+};
 
 LocusZoom.JSONIterator = function(data, keyExtractor, valExtractor) {
 	var index = 0;
 	var done = false;
 
 	this.getKey = function() {
-		val = keyExtractor(data, index)
-			if (!val) {
-				done = true
-			}
-		return(val)
+		var val = keyExtractor(data, index);
+		if (!val) {
+			done = true;
+		}
+		return(val);
+	};
 
-	}
 	this.getValue = function() {
-		return(valExtractor(data, index))
-	}
+		return(valExtractor(data, index));
+	};
 
 	this.moveNext = function() {
 		index += 1;
-	}
+	};
+
 	this.isDone = function() {
 		return(done);
-	}
+	};
 
-}
+};
 
 LocusZoom.ArrayAccumulator = function() {
 	var vals = [];
 	
 	this.add = function(x) {
 		vals.push(x);
-	}
+	};
 	
 	this.getValues = function() {
 		return(vals);
-	}
-}
+	};
+};
 
 LocusZoom.GenomePosComparer = function(a,b, chrVal, posVal) {
 	chrVal = typeof chrVal !== "undefined" ? chrVal : "chr";
 	posVal = typeof posVal !== "undefined" ? posVal : "pos";
 	
-	if (!a) {return(null)}
-	if (!b) {return(null)}	
+	if (!a) {return(null);}
+	if (!b) {return(null);}	
 
 	if (a[[chrVal]] == b[[chrVal]]) {
 		if(a[[posVal]] == b[[posVal]]) {
-			return(0)
+			return(0);
 		} else if (a[[posVal]] < b[[posVal]]) {
-			return(-1)
+			return(-1);
 		} else {
-			return(1)
+			return(1);
 		}
 	} else {
 		if (a[[chrVal]] < b[[chrVal]]) {
-			return(-1)
+			return(-1);
 		} else {
-			return(1)
+			return(1);
 		}
 	}
-}
+};
 
 //from http://www.html5rocks.com/en/tutorials/cors/
 //and with promises from https://gist.github.com/kriskowal/593076
 LocusZoom.createCORSPromise = function (method, url, body, timeout) {
-    var response = Q.defer();
+	var response = Q.defer();
 	var xhr = new XMLHttpRequest();
 	if ("withCredentials" in xhr) {
 
@@ -306,7 +305,7 @@ LocusZoom.createCORSPromise = function (method, url, body, timeout) {
 		// Otherwise, CORS is not supported by the browser.
 		xhr = null;
 	}
-    if (xhr) {
+	if (xhr) {
 		xhr.onreadystatechange = function() {
 			if (xhr.readyState === 4) {
 				if (xhr.status === 200 || xhr.status === 0 ) {
@@ -315,14 +314,14 @@ LocusZoom.createCORSPromise = function (method, url, body, timeout) {
 					response.reject("HTTP" + xhr.status + " for " + url);
 				}
 			}
-		}
+		};
 		timeout && setTimeout(response.reject, timeout);
-		body = typeof body !== "undefined" ? body : ""
+		body = typeof body !== "undefined" ? body : "";
 		xhr.send(body);
 
 	} 
 	return response.promise;
-}
+};
 
 LocusZoom.Default = {};
 LocusZoom.Default.Viewer = LocusZoom.initD3Viewer;
