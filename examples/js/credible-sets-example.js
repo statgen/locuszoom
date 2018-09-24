a8fad220763930c7c1c40ad1fc60082ebeab4331
Andy Boughton <abought@gmail.com>
2018-09-24 15:16:12 -0400

    Credible set reuse improvements
    * Make credible sets more reusable + better display
    option error message
    
    * More consistent layout naming scheme

--- examples/js/credible-sets-example.js
+++ examples/js/credible-sets-example.js
@@ -5,14 +5,13 @@
     The page must incorporate and load all libraries before this file can be used, including:
      - Vendor assets
      - LocusZoom
-     - gwas-credible-sets (available via NPM)
- */
-
+     - gwas-credible-sets (available via NPM or a related CDN)
+*/
 "use strict";
 
 /* global gwasCredibleSets */
 
-// Specify a custom datasource that adds a "credible sets" field to the prepared API response
+// Specify a custom datasource that adds "credible sets" fields to the prepared API response
 LocusZoom.KnownDataSources.extend("AssociationLZ", "CredibleAssociationLZ", {
     annotateData: function (records) {
         // This is a somewhat crude method for adding fields in the front end, after the API response has been returned.
@@ -41,15 +40,14 @@ LocusZoom.KnownDataSources.extend("AssociationLZ", "CredibleAssociationLZ", {
     }
 });
 
-
-LocusZoom.Layouts.add("tooltip", "credible_set_association", function () {
+LocusZoom.Layouts.add("tooltip", "association_credible_set", function () {
     // Extend a known tooltip with an extra row of info showing posterior probabilities
-    var l = LocusZoom.Layouts.get("tooltip", "standard_association", {unnamespaced: true});
+    var l = LocusZoom.Layouts.get("tooltip", "standard_association", { unnamespaced: true });
     l.html += "<br>Posterior probability: <strong>{{{{namespace[assoc]}}credibleSetPosteriorProb|scinotation}}</strong>";
     return l;
 }());
 
-LocusZoom.Layouts.add("tooltip", "credible_set_annotation", {
+LocusZoom.Layouts.add("tooltip", "annotation_credible_set", {
     namespace: {"assoc": "assoc"},
     closable: true,
     show: {or: ["highlighted", "selected"]},
@@ -59,10 +57,25 @@ LocusZoom.Layouts.add("tooltip", "credible_set_annotation", {
     "<br>Posterior probability: <strong>{{{{namespace[assoc]}}credibleSetPosteriorProb|scinotation}}</strong>"
 });
 
-// Define layouts that incorporate credible set annotations into the plot
-LocusZoom.Layouts.add("data_layer", "credible_set_annotation", {
+LocusZoom.Layouts.add("data_layer", "association_credible_set", function () {
+    return LocusZoom.Layouts.get("data_layer", "association_pvalues", {
+        unnamespaced: true,
+        id: "associationcredibleset",
+        fill_opacity: 0.7,
+        tooltip: LocusZoom.Layouts.get("tooltip", "association_credible_set", { unnamespaced: true }),
+        fields: [
+            "{{namespace[assoc]}}variant", "{{namespace[assoc]}}position",
+            "{{namespace[assoc]}}log_pvalue", "{{namespace[assoc]}}log_pvalue|logtoscinotation",
+            "{{namespace[assoc]}}ref_allele", "{{namespace[assoc]}}credibleSetPosteriorProb",
+            "{{namespace[assoc]}}credibleSetContribution", "{{namespace[assoc]}}isCredible",
+            "{{namespace[ld]}}state", "{{namespace[ld]}}isrefvar"
+        ]
+    });
+}());
+
+LocusZoom.Layouts.add("data_layer", "annotation_credible_set", {
     namespace: {"assoc": "assoc"},
-    id: "annotations",
+    id: "annotationcredibleset",
     type: "annotation_track",
     id_field: "{{namespace[assoc]}}variant",
     x_axis: {
@@ -90,12 +103,13 @@ LocusZoom.Layouts.add("data_layer", "credible_set_annotation", {
             {action: "toggle", status: "selected"}
         ]
     },
-    tooltip: LocusZoom.Layouts.get("tooltip", "credible_set_annotation"),
+    tooltip: LocusZoom.Layouts.get("tooltip", "annotation_credible_set", { unnamespaced: true }),
     tooltip_positioning: "vertical"
 });
 
-LocusZoom.Layouts.add("panel", "credible_set_panel", {
-    id: "credible",
+LocusZoom.Layouts.add("panel", "annotation_credible_set", {
+    id: "annotationcredibleset",
+    title: { text: "SNPs in 95% credible set", x:50, style: { "font-size": "14px" } },
     width: 800,
     height: 100,
     min_height: 100,
@@ -108,11 +122,93 @@ LocusZoom.Layouts.add("panel", "credible_set_panel", {
         x_linked: true
     },
     data_layers: [
-        LocusZoom.Layouts.get("data_layer", "credible_set_annotation", {unnamespaced: true})
+        LocusZoom.Layouts.get("data_layer", "annotation_credible_set", {unnamespaced: true})
     ]
 });
 
-LocusZoom.Layouts.add("plot", "association_credible_sets", {
+LocusZoom.Layouts.add("panel", "association_credible_set", function() {
+    var l = LocusZoom.Layouts.get("panel", "association", {
+        unnamespaced: true,
+        id: "associationcrediblesets",
+        namespace: { "assoc": "assoc" },
+        data_layers: [
+            LocusZoom.Layouts.get("data_layer", "significance", { unnamespaced: true }),
+            LocusZoom.Layouts.get("data_layer", "recomb_rate", { unnamespaced: true }),
+            LocusZoom.Layouts.get("data_layer", "association_credible_set", { unnamespaced: true })
+        ]
+    });
+    // Add "display options" button to control how credible set coloring is overlaid on the standard association plot
+    l.dashboard.components.push(
+        {
+            type: "display_options",
+            position: "right",
+            color: "blue",
+            // Below: special config specific to this widget
+            button_html: "Display options...",
+            button_title: "Control how plot items are displayed",
+            layer_name: "associationcredibleset",
+            default_config_display_name: "Linkage Disequilibrium (default)", // display name for the default plot color option (allow user to revert to plot defaults)
+
+            options: [
+                {
+                    // First dropdown menu item
+                    display_name: "95% credible set (boolean)",  // Human readable representation of field name
+                    display: {  // Specify layout directives that control display of the plot for this option
+                        point_shape: "circle",
+                        point_size: 40,
+                        color: {
+                            field: "{{namespace[assoc]}}isCredible",
+                            scale_function: "if",
+                            parameters: {
+                                field_value: true,
+                                then: "#00CC00",
+                                else: "#CCCCCC"
+                            }
+                        },
+                        legend: [ // Tells the legend how to represent this display option
+                            { shape: "circle", color: "#00CC00", size: 40, label: "In credible set", class: "lz-data_layer-scatter" },
+                            { shape: "circle", color: "#CCCCCC", size: 40, label: "Not in credible set", class: "lz-data_layer-scatter" }
+                        ]
+                    }
+                },
+                {
+                    // Second option. The same plot- or even the same field- can be colored in more than one way.
+                    display_name: "95% credible set (gradient by contribution)",
+                    display: {
+                        point_shape: "circle",
+                        point_size: 40,
+                        color: [
+                            {
+                                field: "{{namespace[assoc]}}credibleSetContribution",
+                                scale_function: "if",
+                                parameters: {
+                                    field_value: 0,
+                                    then: "#777777"
+                                }
+                            },
+                            {
+                                scale_function: "interpolate",
+                                field: "{{namespace[assoc]}}credibleSetContribution",
+                                parameters: {
+                                    breaks: [0, 1],
+                                    values: ["#fafe87", "#9c0000"]
+                                }
+                            }
+                        ],
+                        legend: [
+                            { shape: "circle", color: "#777777", size: 40, label: "No contribution", class: "lz-data_layer-scatter" },
+                            { shape: "circle", color: "#fafe87", size: 40, label: "Some contribution", class: "lz-data_layer-scatter" },
+                            { shape: "circle", color: "#9c0000", size: 40, label: "Most contribution", class: "lz-data_layer-scatter" }
+                        ]
+                    }
+                }
+            ]
+        }
+    );
+    return l;
+} ());
+
+LocusZoom.Layouts.add("plot", "association_credible_set", {
     state: {},
     width: 800,
     height: 450,
@@ -121,91 +217,7 @@ LocusZoom.Layouts.add("plot", "association_credible_sets", {
     max_region_scale: 1000000,
     dashboard: LocusZoom.Layouts.get("dashboard", "standard_plot", {unnamespaced: true}),
     panels: [
-        function () {
-            // Use a slightly modified association panel: custom tooltip
-            var assoc_panel = LocusZoom.Layouts.get("panel", "association", {unnamespaced: true});
-
-            // Add "display options" button to control how credible set coloring is overlaid on the standard association plot
-            assoc_panel.dashboard.components.push(
-                {
-                    type: "display_options",
-                    position: "right",
-                    color: "blue",
-                    // Below: special config specific to this widget
-                    button_html: "Display options...",
-                    button_title: "Control how plot items are displayed",
-
-                    layer_name: "associationpvalues",
-                    default_config_display_name: "Linkage Disequilibrium (default)", // display name for the default plot color option (allow user to revert to plot defaults)
-
-                    options: [
-                        {
-                            // First dropdown menu item
-                            display_name: "95% credible set (boolean)",  // Human readable representation of field name
-                            display: {  // Specify layout directives that control display of the plot for this option
-                                point_shape: "circle",
-                                point_size: 40,
-                                color: {
-                                    field: "assoc:isCredible",
-                                    scale_function: "if",
-                                    parameters: {
-                                        field_value: true,
-                                        then: "#00CC00",
-                                        else: "#CCCCCC"
-                                    }
-                                },
-                                legend: [ // Tells the legend how to represent this display option
-                                    { shape: "circle", color: "#00CC00", size: 40, label: "In credible set", class: "lz-data_layer-scatter" },
-                                    { shape: "circle", color: "#CCCCCC", size: 40, label: "Not in credible set", class: "lz-data_layer-scatter" }
-                                ]
-                            }
-                        },
-                        {
-                            // Second option. The same plot- or even the same field- can be colored in more than one way.
-                            display_name: "95% credible set (gradient by contribution)",
-                            display: {
-                                point_shape: "circle",
-                                point_size: 40,
-                                color: [
-                                    { // FIXME: Display options dropdown should support namespacing (namespaces have already been resolved by the time this works, so "assoc" is hardcoded)
-                                        field: "assoc:credibleSetContribution",
-                                        scale_function: "if",
-                                        parameters: {
-                                            field_value: 0,
-                                            then: "#777777"
-                                        }
-                                    },
-                                    {
-                                        scale_function: "interpolate",
-                                        field: "assoc:credibleSetContribution",
-                                        parameters: {
-                                            breaks: [0, 1],
-                                            values: ["#fafe87", "#9c0000"]
-                                        }
-                                    }
-                                ],
-                                legend: [
-                                    { shape: "circle", color: "#777777", size: 40, label: "No contribution", class: "lz-data_layer-scatter" },
-                                    { shape: "circle", color: "#fafe87", size: 40, label: "Some contribution", class: "lz-data_layer-scatter" },
-                                    { shape: "circle", color: "#9c0000", size: 40, label: "Most contribution", class: "lz-data_layer-scatter" }
-                                ]
-                            }
-                        }
-                    ]
-                }
-            );
-            var assoc_layer = assoc_panel.data_layers[2];
-            assoc_layer.tooltip = LocusZoom.Layouts.get("tooltip", "credible_set_association");
-
-            //["{{namespace[assoc]}}variant", "{{namespace[assoc]}}position", "{{namespace[assoc]}}log_pvalue", "{{namespace[assoc]}}credibleSetPosteriorProb", "{{namespace[assoc]}}credibleSetContribution", "{{namespace[assoc]}}isCredible"]
-
-            // assoc_layer.fields.push("{{namespace[assoc]}}credibleSetPosteriorProb", "{{namespace[assoc]}}credibleSetContribution", "{{namespace[assoc]}}isCredible");
-            assoc_layer.fields = [
-                "{{namespace[assoc]}}variant", "{{namespace[assoc]}}position", "{{namespace[assoc]}}log_pvalue", "{{namespace[assoc]}}log_pvalue|logtoscinotation", "{{namespace[assoc]}}ref_allele", "{{namespace[assoc]}}credibleSetPosteriorProb", "{{namespace[assoc]}}credibleSetContribution", "{{namespace[assoc]}}isCredible",
-                "{{namespace[ld]}}state", "{{namespace[ld]}}isrefvar"];
-
-            return assoc_panel;
-        }(),
-        LocusZoom.Layouts.get("panel", "credible_set_panel", {unnamespaced: true})
+        LocusZoom.Layouts.get("panel", "association_credible_set", {unnamespaced: true}),
+        LocusZoom.Layouts.get("panel", "annotation_credible_set", {unnamespaced: true})
     ]
 });
