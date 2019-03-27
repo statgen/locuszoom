d8d10e3a8661fd173b21e582140e5052d92b6d6e
Andy Boughton <abought@gmail.com>
2019-03-27 11:49:08 -0400

    Refactor agg test builder to Vue for easier maintenance

--- examples/js/aggregation-tests-example-page.js
+++ examples/js/aggregation-tests-example-page.js
@@ -3,7 +3,7 @@
  */
 'use strict';
 
-/* global $, raremetal */
+/* global $, raremetal, Vue */
 /* eslint-disable no-unused-vars */
 
 
@@ -12,10 +12,12 @@ var Observable = function () { // Allow UI elements to monitor changes in a vari
     var _subscribers = [];
     var current_value;
     var handle_value = function (value) {
-        if (value === undefined) { return current_value; }
-        if(current_value !== value) {
+        if (value === undefined) {
+            return current_value;
+        }
+        if (current_value !== value) {
             current_value = value;
-            _subscribers.forEach(function(handler) {
+            _subscribers.forEach(function (handler) {
                 try {
                     handler(value);
                 } catch (error) {
@@ -24,12 +26,14 @@ var Observable = function () { // Allow UI elements to monitor changes in a vari
             });
         }
     };
-    handle_value.subscribe = function (handler) { _subscribers.push(handler); };
+    handle_value.subscribe = function (handler) {
+        _subscribers.push(handler);
+    };
     return handle_value;
 };
 
 // Make a custom layout object
-function customizePlotLayout (layout) {
+function customizePlotLayout(layout) {
     // Customize an existing plot layout with the data for aggregation tests
     // Customize layout:
     // 1. The association panel must pull from aggregation tests in order to draw on that data
@@ -66,7 +70,7 @@ function customizePlotLayout (layout) {
 
     // The demo does not have real covariance data, and therefore only works on a narrow plot region. Lock all panels
     //   to prevent scrolling
-    layout.panels.forEach(function(panel_layout) {
+    layout.panels.forEach(function (panel_layout) {
         panel_layout.interaction = {
             drag_background_to_pan: false,
             drag_x_ticks_to_scale: false,
@@ -79,162 +83,26 @@ function customizePlotLayout (layout) {
     return layout;
 }
 
-function _formatSciNotation (cell, params) {
+function _formatSciNotation(cell, params) {
     // Tabulator cell formatter using sci notation
     var value = cell.getValue();
     return LocusZoom.TransformationFunctions.get('scinotation')(value);
 }
 
 // Controllers for page widgets
-
-/**
- * A minimal method of defining aggregation tests in the absence of a UI framework. Proof of concept, ONLY- not intended
- *   for production use. (generating HTML via jquery is rather ugly, but it keeps the examples relatively portable)
- * @class
- * @param {string|object} selector A jquery selector or element name specifying where to draw the widget
- * @param {String[]} mask_names What masks are allowed
- * @param {String[]} [aggregation_names] What tests are recognized.
- */
-var AggregationTestBuilder = LocusZoom.subclass(function() {}, {
-    constructor: function(selector, mask_names, aggregation_names) {
-        // Store the options used to populate the dropdown
-        this._mask_names = mask_names;
-        this._aggregation_types = aggregation_names  || [  // Defaults (correspond to hard-coded serialization logic)
-            ['burden', 'Burden'],
-            ['skat', 'SKAT']
-        ];
-
-        if (typeof selector === 'string') {
-            selector = $(selector);
-        }
-
-        selector.html('');
-        this._container = selector;
-        this._aggregation_spec_list_container = $('<div></div>').appendTo(selector);
-
-        // Make sure that at least one set of test-description input elements appears on first render
-        this.addControls();
-        this._status_div = $('<div></div>').css('color', 'red;').appendTo(selector);
-    },
-
-    addControls: function() {
-        // Build these fragments once and reuse
-        var _mask_choices = this.__render_selection('mask_choice', this._mask_names);  // Assume this comes from an API / remote source
-        var _calc_choices = this.__render_checkboxes('calc_choice', this._aggregation_types);
-
-        this._aggregation_spec_list_container.append(this.__form_row('Select mask(s)', _mask_choices));
-        this._aggregation_spec_list_container.append(this.__form_row('Select test(s)', _calc_choices));
-    },
-
-    // Display a (styled) status message to the user. Default styling is an error message.
-    setStatus: function (message, css) {
-        css = css || { color: 'red' };
-        this._status_div
-            .text(message || '')
-            .css(css);
-    },
-
-    __form_row: function (label_text, controls_el) {
-        var row = $('<div></div>', {class: 'row'});
-        var label = $('<div></div>', {class: 'two columns'}).css('font-weight', 'bold').text(label_text);
-        var content = $('<div></div>', {class: 'ten columns'});
-        content.append(controls_el);
-        row.append(label, content);
-        return row;
-    },
-
-    /**
-     *
-     * @param {String} name The name of the select menu
-     * @param {String[]} options An array where each element specifies [value, displayName]
-     * @private
-     */
-    __render_selection: function (name, options) {
-        var htmlescape = LocusZoom.TransformationFunctions.get('htmlescape');
-        var element = $('<select></select>', { name: name, size: 5 }).prop('multiple', true).css('height', 'auto');
-
-        options = options.slice();
-
-        options.forEach(function(option) {
-            var value;
-            var displayName;
-            if (Array.isArray(option)) {  // Optionally specify a second, human readable name
-                value = option[0];
-                displayName = option[1];
-            } else {
-                value = displayName = option;
-            }
-            var choice = $('<option></option>'  , { value: htmlescape(value) }).text(displayName);
-            element.append(choice);
-        });
-        return element;
-    },
-
-    /**
-     *
-     * @param {String} name The name attribute for all checkboxes in this group
-     * @param {String[]} options An array where each element specifies [value, displayName]
-     * @return {Element}
-     * @private
-     */
-    __render_checkboxes: function(name, options) {
-        var htmlescape = LocusZoom.TransformationFunctions.get('htmlescape');
-        var element = $('<div></div>');
-
-        options = options.slice();
-        options.forEach(function(option) {
-            var value;
-            var displayName;
-            if (Array.isArray(option)) {  // Optionally specify a second, human readable name
-                value = option[0];
-                displayName = option[1];
-            } else {
-                value = displayName = option;
-            }
-            var wrapper = $('<label></label>').css('display', 'inline');
-            var control = $('<input>', { type: 'checkbox', name: name, value: htmlescape(value) });
-            var label = $('<span></span>', { class: 'label-body'}).text(displayName);
-
-            wrapper.append(control, label);
-            element.append(wrapper);
-        });
-        return element;
-    },
-
-    /**
-     * Must select at least one item from each box
-     * @returns {boolean}
-     */
-    validate: function(calcs, masks) {
-        // all test names unique across tests + all fields filled in
-        calcs = calcs || this.getCalcs();
-        masks = masks || this.getMasks();
-        return calcs.length && masks.length;
-    },
-
-    getMasks: function() {
-        var masks = this._aggregation_spec_list_container.find("[name='mask_choice']").find(':selected');
-        return masks.map(function() { return this.value; }).get();
-    },
-
-    getCalcs: function() {
-        var masks = this._aggregation_spec_list_container.find("[name='calc_choice']:checked");
-        return masks.map(function() { return this.value; }).get();
-    }
-});
-
 /**
  * Define shared functionality for all tables, providing helper methods to control the tabulator
  *  table library
  * @class
  */
-var GenericTabulatorTableController = LocusZoom.subclass(function() {}, {
+var GenericTabulatorTableController = LocusZoom.subclass(function () {
+}, {
     /**
      *
      * @param {string|Object} selector A selector string for the table container
      * @param {object} table_config An object specifying the tabulator layout for this table
      */
-    constructor: function(selector, table_config) {
+    constructor: function (selector, table_config) {
         if (typeof selector === 'string') {
             selector = $(selector);
         }
@@ -257,9 +125,11 @@ var GenericTabulatorTableController = LocusZoom.subclass(function() {}, {
      * @param data
      * @returns {*}
      */
-    prepareData: function (data) { return data; },
+    prepareData: function (data) {
+        return data;
+    },
 
-    renderData: function(data) {
+    renderData: function (data) {
         data = this.prepareData(data);
         this._tableUpdateData(data);
     },
@@ -277,7 +147,7 @@ var GenericTabulatorTableController = LocusZoom.subclass(function() {}, {
 
     },
 
-    tableDownloadData: function(filename, format) {
+    tableDownloadData: function (filename, format) {
         format = format || 'csv';
         this.selector.tabulator('download', format, filename);
     }
@@ -308,20 +178,28 @@ function createDisplayWidgets(label_store, context) {
 
     // Specify the data sources to use, then build the plot
     var apiBase = '//portaldev.sph.umich.edu/api/v1/';
-    var data_sources =  new LocusZoom.DataSources()
-        .add('aggregation', ['AggregationTestSourceLZ', {url: 'data/scorecov.json'}])
+    var data_sources = new LocusZoom.DataSources()
+        .add('aggregation', ['AggregationTestSourceLZ', { url: 'data/scorecov.json' }])
         .add('assoc', ['AssocFromAggregationLZ', {  // Use a special source that restructures already-fetched data
             from: 'aggregation',
             params: { id_field: 'variant' }
         }])
-        .add('ld', ['LDLZ2', { url: 'https://portaldev.sph.umich.edu/ld/', params: { source: '1000G', build: 'GRCh37', population: 'ALL' } }])
-        .add('gene', ['GeneLZ', {url: apiBase + 'annotation/genes/', params: { build: 'GRCh37' }}])
-        .add('aggregation_genes', ['GeneAggregationConnectorLZ', {sources: {aggregation_ns: 'aggregation', gene_ns: 'gene'}}])
-        .add('recomb', ['RecombLZ', {url: apiBase + 'annotation/recomb/results/', params: { build: 'GRCh37' }}])
-        .add('constraint', ['GeneConstraintLZ', {url: 'http://exac.broadinstitute.org/api/constraint'}]);  // FIXME: use https when exac fixed
+        .add('ld', ['LDLZ2', {
+            url: 'https://portaldev.sph.umich.edu/ld/',
+            params: { source: '1000G', build: 'GRCh37', population: 'ALL' }
+        }])
+        .add('gene', ['GeneLZ', { url: apiBase + 'annotation/genes/', params: { build: 'GRCh37' } }])
+        .add('aggregation_genes', ['GeneAggregationConnectorLZ', {
+            sources: {
+                aggregation_ns: 'aggregation',
+                gene_ns: 'gene'
+            }
+        }])
+        .add('recomb', ['RecombLZ', { url: apiBase + 'annotation/recomb/results/', params: { build: 'GRCh37' } }])
+        .add('constraint', ['GeneConstraintLZ', { url: 'http://exac.broadinstitute.org/api/constraint' }]);  // FIXME: use https when exac fixed
 
-    var initialState = {chr: 15, start: 58384122, end: 59305748};
-    var layout = LocusZoom.Layouts.get('plot', 'standard_association', {state: initialState});
+    var initialState = { chr: 15, start: 58384122, end: 59305748 };
+    var layout = LocusZoom.Layouts.get('plot', 'standard_association', { state: initialState });
     layout = customizePlotLayout(layout);
 
     var plot = LocusZoom.populate('#lz-plot', data_sources, layout);
@@ -334,7 +212,7 @@ function createDisplayWidgets(label_store, context) {
         height: 300,
         layout: 'fitColumns',
         layoutColumnsOnNewData: true,
-        rowSelected: function(row) {
+        rowSelected: function (row) {
             label_store(row.row.data); // Tabulator doesn't allow changing options after creation
         },
         rowDeselected: function () {
@@ -400,7 +278,7 @@ function createDisplayWidgets(label_store, context) {
  * @param {Observable} labelStorage Observable used to label the selected group
  */
 function setupWidgetListeners(plot, aggregationTable, variantsTable, resultStorage, labelStorage) {
-    plot.on('element_selection', function(eventData) {
+    plot.on('element_selection', function (eventData) {
         // Trigger the aggregation test table to filter (or unfilter) if a specific gene on the genes panel is clicked
         if (eventData['sourceID'] !== 'lz-plot.genes') {
             return;
@@ -441,11 +319,11 @@ function setupWidgetListeners(plot, aggregationTable, variantsTable, resultStora
             // The aggregation results use the unique ENSEMBL ID for a gene. The gene source tells us how to connect
             //  that to a human-friendly gene name (as displayed in the LZ plot)
             var _genes_lookup = {};
-            gene_source_data.forEach(function(gene) {
+            gene_source_data.forEach(function (gene) {
                 var gene_id = gene.gene_id.split('.')[0]; // Ignore ensembl version on gene ids
                 _genes_lookup[gene_id] = gene.gene_name;
             });
-            groups.data.forEach(function(one_result) {
+            groups.data.forEach(function (one_result) {
                 var this_group = groups.getOne(one_result.mask, one_result.group);
                 // Add synthetic fields that are not part of the raw calculation results
                 one_result.group_display_name = _genes_lookup[one_result.group] || one_result.group;
@@ -488,11 +366,61 @@ function setupWidgetListeners(plot, aggregationTable, variantsTable, resultStora
 
     //////////////////////////////////////////////////////////////
     // Generic UI controls: what to do when buttons are clicked
-    $('#download-aggregation').on('click', function() {
+    $('#download-aggregation').on('click', function () {
         aggregationTable.tableDownloadData('aggregation-data.csv', 'csv');
     });
 
-    $('#download-variants').on('click', function() {
+    $('#download-variants').on('click', function () {
         variantsTable.tableDownloadData('variants-data.csv', 'csv');
+    });
+}
+
+
+function makeUI(selector, masks, phenotypes) {
+    // The UI is written in Vue.js. Although modern tooling can be nice, this example can be reused via plain JS.
+    return new Vue({
+        el: selector,
+        data: function () {
+            return {
+                // options passed in at creation
+                masks: masks,
+                phenotypes: phenotypes,
+                // categories: ['Cardiovascular'],
+                calc_names: [
+                    ['burden', 'Burden'],
+                    ['skat', 'SKAT']
+                ],
+                // Tracking internal state
+                status_css: { color: 'red' },
+                status_message: null,
+
+                // API supports one pheno/multiple masks/ multiple tests
+                // TODO: Once live server returns, write UI for "category--> phenotypes" (using optgroup)
+                selected_phenotype: null,
+                selected_masks: [],
+                selected_tests: [],
+            };
+        },
+        methods: {
+            setStatus: function (message, success) {
+                this.status_message = message;
+                this.status_css.color = success ? 'green' : 'red';
+            },
+            isValid: function () {
+                //return //this.selected_phenotype &&
+                return this.selected_masks.length && this.selected_tests.length;
+            },
+            runTests: function () {
+                var status = this.isValid();
+                this.setStatus(status ? '' : 'Please select at least one option from each category', status);
+                if (status) {
+                    this.$emit('run', {
+                        masks: this.selected_masks,
+                        calcs: this.selected_tests,
+                        pheno: this.selected_phenotype
+                    });
+                }
+            }
+        },
     });
 }
