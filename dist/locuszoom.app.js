6e91e826086d1229fd6dd4940693ee312b9ceb31
Andy Boughton <abought@gmail.com>
2019-05-02 00:38:41 -0400

    Commit built assets

--- dist/locuszoom.app.js
+++ dist/locuszoom.app.js
@@ -8849,13 +8849,25 @@
             return data;
         };
         LocusZoom.Data.LDSource.prototype.getRefvar = function (state, chain, fields) {
-            var findExtremeValue = function (x, pval, sign) {
-                pval = pval || 'pvalue';
-                sign = sign || 1;
-                var extremeVal = x[0][pval], extremeIdx = 0;
-                for (var i = 1; i < x.length; i++) {
-                    if (x[i][pval] * sign > extremeVal) {
-                        extremeVal = x[i][pval] * sign;
+            var findExtremeValue = function (records, pval_field) {
+                // Finds the most significant hit (smallest pvalue, or largest -log10p). Will try to auto-detect the appropriate comparison.
+                pval_field = pval_field || 'log_pvalue';
+                // The official LZ API returns log_pvalue
+                var is_log = /log/.test(pval_field);
+                var cmp;
+                if (is_log) {
+                    cmp = function (a, b) {
+                        return a > b;
+                    };
+                } else {
+                    cmp = function (a, b) {
+                        return a < b;
+                    };
+                }
+                var extremeVal = records[0][pval_field], extremeIdx = 0;
+                for (var i = 1; i < records.length; i++) {
+                    if (cmp(records[i][pval_field], extremeVal)) {
+                        extremeVal = records[i][pval_field];
                         extremeIdx = i;
                     }
                 }
