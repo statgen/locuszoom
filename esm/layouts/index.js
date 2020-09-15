ec0434ea66c6b91401bc519cb542af1cc224b752
Andy Boughton <abought@gmail.com>
2020-09-15 11:17:44 -0400

    Change color scheme for arcs demo to better balance aesthetics in dense regions
    (there are so many loops that trying to assign rigorously separate category colors just created visual noise)

--- esm/layouts/index.js
+++ esm/layouts/index.js
@@ -221,7 +221,8 @@ const coaccessibility_layer = {
         {
             scale_function: 'ordinal_cycle',
             parameters: {
-                values: ['#1f77b4', '#aec7e8', '#ff7f0e', '#ffbb78', '#2ca02c', '#98df8a', '#d62728', '#ff9896', '#9467bd', '#c5b0d5', '#8c564b', '#c49c94', '#e377c2', '#f7b6d2', '#7f7f7f', '#c7c7c7', '#bcbd22', '#dbdb8d', '#17becf', '#9edae5'], // Drawn from d3v3 "category20"
+                // Modified from colorbrewer 9-class GnBu (balance separate categories with aesthetics)
+                values: ['#ccebc5', '#a8ddb5', '#7bccc4', '#4eb3d3', '#2b8cbe', '#0868ac', '#084081'],
             },
         },
     ],
