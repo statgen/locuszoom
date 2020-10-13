34e0c6fed64d810047ff06bc24524ff69ceb572d
Andy Boughton <abought@gmail.com>
2020-10-13 16:05:30 -0400

    Revert colormap for arcs; blues have insufficient contrast

--- esm/components/data_layer/base.js
+++ esm/components/data_layer/base.js
@@ -1225,7 +1225,7 @@ class BaseDataLayer {
             // This method may be used on two kinds of events: directly attached, or bubbled.
             // D3 doesn't natively support bubbling very well; if no data is bound on the currentTarget, check to see
             //  if there is data available at wherever the event was initiated from
-            element = d3.select(d3.event.target).datum();
+            element = element || d3.select(d3.event.target).datum();
 
             // Do nothing if the required control and shift key presses (or lack thereof) doesn't match the event
             if (requiredKeyStates.ctrl !== !!d3.event.ctrlKey || requiredKeyStates.shift !== !!d3.event.shiftKey) {
